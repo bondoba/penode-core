85e44bd43c8284bd629b463df7d5bf9b7a960d33
bondoba <86989453+bondoba@users.noreply.github.com>
2021-07-10 01:30:09 +0100

    (Draft) Put everything into index for now
    will not stay like that I promise. Btw this commit is
    not finished though!!1!1!

--- index.ts
+++ index.ts
@@ -17,7 +17,67 @@
 *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 
-class Portfolio {
-  id: string
-  _ownerId: string
+function createId(type: IdType): string {
+  let boilerId: string
+  switch(type) {
+    case(IdType.OWNER):
+      boilerId = 'own-aaa-abbbb-abb-ba'
+      break
+    case(IdType.POT):
+      boilerId = 'pot-ab-bbbab-bab-bb'
+      break
+    default:
+      boilerId = 'bbaabbaa'
+      break
+  }
+  const numRepId = boilerId.replace(/b/g, () => (Math.floor(Math.random() * 9) + 1).toString())
+  const newId = numRepId.replace(/a/g, () => String.fromCharCode(97 + Math.floor(Math.random() * 26)))
+  return duplicateDbCheckMock(newId) !== 0 ? createId(type) : newId
+}
+
+function duplicateDbCheckMock(id: string): number {
+  const idToCheck = new String(id)
+  idToCheck.toLowerCase()
+  return Math.floor(Math.random() * 2)
+}
+
+enum IdType {
+  OWNER = 0,
+  POT
+}
+
+enum OwnerType {
+  PLACEHOLDER_A = 0,
+  PLACEHOLDER_B,
+  PLACEHOLDER_C
+}
+
+enum PotType {
+  CASH = 0,
+  STOCKS,
+  CRYPTO,
+  CHEESE
+}
+
+class Owner {
+  private id: string
+  private _potIds: string[]
+  private name?: string
+  private ownerTypes?: OwnerType[]
+  constructor() {
+    this.id = createId(IdType.OWNER)
+    this._potIds = []
+  }
+}
+
+abstract class Pot {
+  private id: string
+  private _ownerId: string
+  private name?: string
+  private type: PotType
+  private isSubPot: boolean
+  constructor(ownerId: string) {
+    this.id = createId(IdType.POT)
+    this._ownerId = ownerId
+  }
 }
