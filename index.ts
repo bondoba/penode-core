ed63b151bde9b71be862193702cbe056ca98ee0d
bondoba <86989453+bondoba@users.noreply.github.com>
2021-07-14 01:29:51 +0100

    Implement basic relationship system

--- index.ts
+++ index.ts
@@ -20,22 +20,20 @@
 function createId(type: IdType): string {
   let boilerId: string
   switch(type) {
+    default:
     case(IdType.OWNER):
       boilerId = 'own-aaa-abbbb-abb-ba'
       break
     case(IdType.POT):
       boilerId = 'pot-ab-bbbab-bab-bb'
       break
-    default:
-      boilerId = 'bbaabbaa'
-      break
   }
   const numRepId = boilerId.replace(/b/g, () => (Math.floor(Math.random() * 9) + 1).toString())
   const newId = numRepId.replace(/a/g, () => String.fromCharCode(97 + Math.floor(Math.random() * 26)))
-  return duplicateDbCheckMock(newId) !== 0 ? createId(type) : newId
+  return duplicateDbCheckMock(newId, type) !== 0 ? createId(type) : newId
 }
 
-function duplicateDbCheckMock(id: string): number {
+function duplicateDbCheckMock(id: string, type: IdType): number {
   const idToCheck = new String(id)
   idToCheck.toLowerCase()
   return Math.floor(Math.random() * 2)
@@ -52,11 +50,23 @@ enum OwnerType {
   PLACEHOLDER_C
 }
 
-enum PotType {
-  CASH = 0,
-  STOCKS,
-  CRYPTO,
-  CHEESE
+enum ContentType {
+  CASH = 'cash',
+  STOCKS = 'stocks'
+}
+
+enum ContentUnit {
+  PIECES = 'pcs',
+  GRAMS = 'gr',
+  DECIMALNUMBER = 'numeric'
+}
+
+enum CurrencySym {
+  USD = 'USD',
+  CAD = 'CAD',
+  EUR = 'EUR',
+  GBP = 'GBP',
+  AUD = 'AUD'
 }
 
 class Owner {
@@ -70,14 +80,40 @@ class Owner {
   }
 }
 
+type PotContent = {
+  kind: ContentType
+  unit: ContentUnit
+  quantity: number
+}
+
 abstract class Pot {
   private id: string
   private _ownerId: string
-  private name?: string
-  private type: PotType
+  private displayCurrency: CurrencySym
   private isSubPot: boolean
-  constructor(ownerId: string) {
+  protected contents: PotContent[]
+  private name?: string
+  private _supPotId?: string
+  constructor(ownerId: string, displayCurrency: CurrencySym = CurrencySym.USD, isSubPot: boolean = false, supPotId: string = null) {
     this.id = createId(IdType.POT)
     this._ownerId = ownerId
+    this.isSubPot = isSubPot
+    if (isSubPot) this._supPotId = supPotId
+    this.contents = []
+  }
+  setName(newName: string): void {
+    this.name = newName
+  }
+  abstract validateNewContent(newContent: PotContent): boolean
+}
+
+class CashPot extends Pot {
+  validateNewContent(newContent: PotContent): boolean {
+    if (newContent.kind !== ContentType.CASH && newContent.quantity <= 0 && newContent.unit !== ContentUnit.DECIMALNUMBER) {
+      return false
+    } else {
+      this.contents.push(newContent)
+      return true
+    }
   }
 }
